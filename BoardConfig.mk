c2ca7964796e33eaed9c6e8dbe4e11cea8c76b63
Aman Kumar <firelord.xda@gmail.com>
2016-05-17 13:45:53 +0530

    hllte: Add bt header

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,5 +11,8 @@ TARGET_KERNEL_CONFIG := cyanogenmod-exynos5260-hlltexx_00_defconfig
 #BOARD_RIL_CLASS := ../../../device/samsung/hllte/ril
 BOARD_MODEM_TYPE := xmm6360
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/hllte/bluetooth
+
 # Inherit common board flags
 -include device/samsung/smdk5260-common/BoardConfigCommon.mk
