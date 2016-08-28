6242187f8d3651a04886cce981f991c57b596f97
FireLord1 <firelord.xda@gmail.com>
2016-08-28 19:09:53 +0530

    smdk5260: remove NFC
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- cm.mk
+++ cm.mk
@@ -11,9 +11,6 @@ PRODUCT_RELEASE_NAME := hllte
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Nfc
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Override build properties.
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hlltexx \
