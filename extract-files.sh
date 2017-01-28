51c6d6e88a6a6142656bec63916ab9cf1cd6d1cb
FireLord1 <firelord.xda@gmail.com>
2017-01-28 11:29:53 +0530

    hllte: Use new extract utils from moto8916
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -1,55 +1,25 @@
-#!/bin/sh
-
-VENDOR=samsung
-DEVICE=hllte
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 set -e
 
-if [ $# -eq 0 ]; then
-  SRC=adb
-else
-  if [ $# -eq 1 ]; then
-    SRC=$1
-  else
-    echo "$0: bad number of arguments"
-    echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-    echo ""
-    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-    echo "the device using adb pull."
-    exit 1
-  fi
-fi
+# Required!
+export DEVICE=hllte
+export DEVICE_COMMON=smdk5260-common
+export VENDOR=samsung
 
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
-
-if [ -f ../$DEVICE/device-proprietary-files.txt ]; then
-  for FILE in `egrep -v '(^#|^$)' ../$DEVICE/device-proprietary-files.txt`; do
-    echo "Extracting /system/$FILE ..."
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-      mkdir -p $BASE/$DIR
-    fi
-    if [ "$SRC" = "adb" ]; then
-      adb pull /system/$FILE $BASE/$FILE
-    else
-      cp $SRC/system/$FILE $BASE/$FILE
-    fi
-  done
-fi
-
-for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
-  echo "Extracting /system/$FILE ..."
-  DIR=`dirname $FILE`
-  if [ ! -d $BASE/$DIR ]; then
-    mkdir -p $BASE/$DIR
-  fi
-  if [ "$SRC" = "adb" ]; then
-    adb pull /system/$FILE $BASE/$FILE
-  else
-    cp $SRC/system/$FILE $BASE/$FILE
-  fi
-done
-
-./setup-makefiles.sh
+./../../$VENDOR/$DEVICE_COMMON/extract-files.sh $@
