369f9eebab9ca33798124fe196c4c9d80652751e
FireLord1 <firelord.xda@gmail.com>
2017-02-20 01:38:22 +0530

    hllte: move cbd to system
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- device.mk
+++ device.mk
@@ -1,10 +1,6 @@
 # Local Path
 LOCAL_PATH := device/samsung/hllte
 
-# Radio
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/sbin/cbd:root/sbin/cbd
-
 # Ramdisk
 PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/rootdir/init.target.rc:root/init.target.rc \
